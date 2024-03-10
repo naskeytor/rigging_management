b2ab36fa5effd8a837259bf85ae2cda6f6edfdaa
Atanas Turlakov <verticalwind@gmail.com>
2024-03-10 17:50:55 +0100

    Add user class

--- rigging_test/app.py
+++ rigging_test/app.py
@@ -1,15 +1,50 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from extensions import db, migrate
-from models import Manufacturer, Size, Status, ComponentType, Model, Component, Rig
+from models import Manufacturer, Size, Status, ComponentType, Model, Component, Rig, User
 from utilities import find_component_by_serial, prepare_component_data
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3664atanas'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:3664atanas@localhost:3306/rigging'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 db.init_app(app)
 migrate.init_app(app, db)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        # Redirige o maneja el flujo post-registro
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Nombre de usuario o contraseña incorrectos.', 'danger')
+    return render_template('login.html')
+
 
 
 @app.route('/')
